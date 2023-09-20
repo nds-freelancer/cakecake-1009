2f177fdc3c9c7aa0d0d306b52ed648788ecc0660
D.Son <nds.freelancer@gmail.com>
2023-09-20 11:29:01 +0700

    11h28

--- src/main/java/com/marufhassan/cmsshoppingcart/controllers/PagesController.java
+++ src/main/java/com/marufhassan/cmsshoppingcart/controllers/PagesController.java
@@ -1,18 +1,25 @@
 package com.marufhassan.cmsshoppingcart.controllers;
 
+import com.marufhassan.cmsshoppingcart.models.PageRepository;
+import com.marufhassan.cmsshoppingcart.models.data.Page;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class PagesController {
+    @Autowired
+    private PageRepository pageRepo;
 
     @GetMapping
     public String home(Model model) {
-
+        Page page = pageRepo.findBySlug("home");
+        model.addAttribute("page", page);
         return "page";
     }
 
@@ -20,13 +27,15 @@ public class PagesController {
     public String login() {
         return "login";
     }
-	@GetMapping("/admin") public String user()
-	{
-		return "admin.html";
-	}
 
-	@GetMapping("/basic") public String basic()
-	{
-		return "basic.html";
-	}
+
+    @GetMapping("/{slug}")
+    public String page(@PathVariable String slug, Model model) {
+        Page page = pageRepo.findBySlug(slug);
+        if (page == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("page", page);
+        return "page";
+    }
 }
